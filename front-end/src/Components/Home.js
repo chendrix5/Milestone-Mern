43b5f920d45d610782806643efe3ea241cba5263
Jake Casartelli <casartellijake@yahoo.com>
2023-01-07 12:18:40 -0500

    added file structure

---
+++ front-end/src/Components/Home.js
@@ -0,0 +1,11 @@
+export default function Home (){
+    return(
+        <div className="homepage">
+            <h1>Vacation Spots</h1>
+            
+            <h2> Welcome to a new website that is a pool of vaction spots that anyone can add to!</h2>
+
+            <h2>You can browse new vacation spots or add your own vacation that youve had to help users find a new vactaion!</h2>
+        </div>
+    )
+}
