af6ba435a8db043c56dfd8d70f3eb5e14d5df2d6
Chloe Kurdziel <chloekurdziel@gmail.com>
2023-01-07 13:15:16 -0500

    updated app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Home from './components/Home'
+import Vacations from './components/Vacations'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Take a look at some cool vacation spots!</h1>
+        
+        <div className="navbar">
+          <ul>
+            <li>
+              <a href={<Home />}>Home</a>
+            </li>
+            <li>
+              <a href={<Vacations />}></a>
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
